23878efebdefccc253a3aa8a60295a073f448f02
LY QUACH <lyllq.quach@gmail.com>
2022-02-05 19:09:39 +0100

    CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
+  def index
+    @article = article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+  
 end
